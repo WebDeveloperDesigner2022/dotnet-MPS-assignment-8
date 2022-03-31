cda8e92a814ed9cea42931a2f32463f65ee2609a
WebDeveloperDesigner2022 <4275876@myuwc.ac.za>
2022-03-31 11:23:54 +0200

    My first commit

---
+++ MPS Assignment 8/BeerSong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace MPS_Assignment_8
+{
+    internal class BeerSong
+    {
+        private int _index;
+        public BeerSong(int index)
+        {
+            _index = index;
+        }  
+
+        public int Index { 
+            get { return _index; }
+            set { _index = value; }
+        }
+
+        public void Sing()
+        {
+            string songLines = "";
+            
+            for (int i = 1; i <= _index; i++)
+            {
+                songLines = $"{_index - i} bottles of beer on the wall, {_index - i}  bottles of beer.\nTake one down and pass it around, {_index - i} bottles of beer on the wall.";
+                Console.WriteLine(songLines);
+            }
+        }
+
+    }
+}
---
+++ MPS Assignment 8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MPS_Assignment_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = 100;
+            BeerSong song = new BeerSong(range);
+            song.Sing();
+        }
+    }
+}
